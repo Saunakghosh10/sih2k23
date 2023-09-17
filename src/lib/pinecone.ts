ce9bdc2b4f6b6e76b100b34c49e813480297a7f9
Saunak Ghosh <saunakghosh710@gmail.com>
2023-09-17 23:55:27 +0530

    commit

---
+++ src/lib/pinecone.ts
@@ -0,0 +1,33 @@
+import {
+  PineconeClient,
+  Vector,
+  utils as PineconeUtils,
+} from "@pinecone-database/pinecone";
+import { downloadFromS3 } from "./s3-server";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+
+let pinecone: PineconeClient | null = null;
+
+export const getPineconeClient = async () => {
+  if (!pinecone) {
+    pinecone = new PineconeClient();
+    await pinecone.init({
+      environment: process.env.PINECONE_ENVIRONMENT!,
+      apiKey: process.env.PINECONE_API_KEY!,
+    });
+  }
+  return pinecone;
+};
+
+export async function loadS3IntoPinecone(fileKey: string) {
+  console.log("downloading s3 into file system");
+  const file_name = await downloadFromS3(fileKey);
+  if (!file_name) {
+    throw new Error("could not download from s3");
+  }
+  const loader = new PDFLoader(file_name);
+
+  const pages = await loader.load();
+  return pages ; 
+
+}
